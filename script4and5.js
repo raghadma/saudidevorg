2ea677c6269dc577e55ad9df905ba61f1fc16fbb
Raghad M <32623040+raghadma@users.noreply.github.com>
2019-08-25 23:18:03 +0300

    day4and5

---
+++ script4and5.js
@@ -0,0 +1,7 @@
+//by raghad alsaleh
+//bay 4 and 5
+
+
+var x =10
+
+document.write("Hello, this is my first challenge and I will do it "+ x +" times. "+ "<br/>" + " I will do it, I will learn JavaScript.")
